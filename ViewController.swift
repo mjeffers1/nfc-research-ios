a2187bc756d837d067ab8fdb132624853f8632ef
Martin Jeffers <martin.jeffers1@gmail.com>
2018-02-08 15:59:18 -0500

    Fleshed out controller.
    Converted the controller to a UITableViewController and implemented the delegate functions.

--- ViewController.swift
+++ ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import CoreNFC
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    fileprivate var session: NFCNDEFReaderSession?
+    fileprivate var messages: [[NFCNDEFMessage]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.messages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.messages[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let messageCount = self.messages[section].count
+        let headerTitle = messageCount == 1 ? "One Message" : "\(messageCount) Messages"
+        
+        return headerTitle
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as UITableViewCell
+        let nfcTag = self.messages[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = "\(nfcTag.records.count) Records"
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let nfcTag = self.messages[indexPath.section][indexPath.row]
+        let records = nfcTag.records.map({ String(describing: String(data: $0.payload, encoding: .utf8)!) })
+        
+        let alertTitle = " \(nfcTag.records.count) Records found in Message"
+        let alert = UIAlertController(title: alertTitle, message: records.joined(separator: "\n"), preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
